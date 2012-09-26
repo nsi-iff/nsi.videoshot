d8282cf92b732705b5f677e1ec4752ee75ab43d0
whanderley <whanderley.souza@gmail.com>
2012-09-26 16:29:45 -0300

    add test for generation thumbnails

--- tests/spec_granularization.py
+++ tests/spec_granularization.py
@@ -13,5 +13,6 @@ class SegmentationSpec(unittest.TestCase):
 		input_video_path = os.path.join(os.getcwd(), 'resources/test.avi')
 		videoSegmentation.video_shot(['videoShot', '-i', input_video_path, '-o', temp])
 		len(os.listdir(os.path.join(temp + '/segmentation_video/parts_videos'))) |should| equal_to(7)
+		len(os.listdir(os.path.join(temp + '/segmentation_video/thumbnails'))) |should| equal_to(7)
 		len(os.listdir(os.path.join(temp + '/segmentation_video/transitions_video'))) |should| equal_to(23)
 		len(os.listdir(os.path.join(temp + '/segmentation_video/video_audio'))) |should| equal_to(1)
